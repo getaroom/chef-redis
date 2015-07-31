7da4f0f0a74c7fa18b62c6c550db4e7e3acff864
Morgan Nelson <mnelson@steele-forge.com>
2015-07-31 09:35:31 -0500

    Update metadata.rb
    name is a required attribute in the metadata now

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,7 @@ maintainer_email "scott@likens.us"
 license          "Apache 2.0"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "4.0.01"
+name             "redis"
 
 description      "Redis: a fast, flexible datastore offering an extremely useful set of data structure primitives"
 
